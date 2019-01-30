d89941c74223b280d0f889821e33a5c84d828494
Yakov Dalinchuk <yakov.dalinchuk@yandex.ru>
2019-01-30 07:58:09 +0000

    Forms added

--- examples/js/index.js
+++
@@ -1,8 +0,0 @@
-// console.log(document.location.href);
-// var menuLinks = document.getElementsByClassName('menu-link');
-// Array.prototype.forEach.call(menuLinks, link => {
-//   console.dir(link.href);
-//   if (link.href === document.location.href) {
-//     link.className = 'menu-link menu-link--active';
-//   }
-// });
