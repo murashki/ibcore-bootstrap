ae86e3b65a0b2650587c5ecae620f0bef8a2148d
Yakov Dalinchuk <yakov.dalinchuk@yandex.ru>
2019-08-17 22:45:45 +0300

    Handlebars added

---
+++ scripts/compile-examples.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+const Handlebars = require('handlebars');
+
+const TEMPLATE_DIR = './src/examples';
+const OUTPUT_DIR = './examples';
+const STANDALONE_OUTPUT_DIR = OUTPUT_DIR + '/standalone';
+
+const CONFIG_PATH = TEMPLATE_DIR + '/config.json';
+
+const CONTENT_DIR = TEMPLATE_DIR + '/content';
+const LAYOUT_DIR = TEMPLATE_DIR + '/layout';
+const DEFAULT_LAYOUT_PATH = LAYOUT_DIR + '/default.hbs';
+const STANDALONE_LAYOUT_PATH = LAYOUT_DIR + '/standalone.hbs';
+
+function render(filename, data) {
+  const source = fs.readFileSync(filename, 'utf8');
+  return Handlebars.compile(source)(data);
+}
+
+const config = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'));
+
+config.pages.forEach(function (page) {
+  const data = Object.assign({ pages: config.pages }, page);
+  const templateFileName = page.key + '.hbs';
+  const pageFileName = page.key + '.html';
+
+  const content = fs.readFileSync(path.join(CONTENT_DIR, templateFileName), 'utf8');
+  Handlebars.registerPartial('content', content);
+
+  Handlebars.registerHelper('activePage', function (key, options) {
+    return key === page.key ? options.fn(this) : options.inverse(this);
+  });
+
+  const defaultPage = render(DEFAULT_LAYOUT_PATH, data);
+  const standalonePage = render(STANDALONE_LAYOUT_PATH, data);
+
+  fs.writeFileSync(path.join(OUTPUT_DIR, pageFileName), defaultPage);
+  fs.writeFileSync(path.join(STANDALONE_OUTPUT_DIR, pageFileName), standalonePage);
+});
